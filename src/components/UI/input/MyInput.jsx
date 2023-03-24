f5658de3c075d698ea5d7b5922416e689c09ed9c
Pavel Karpov <paszakarpow@yandex.ru>
2023-03-25 00:32:58 +0500

    first commit

---
+++ src/components/UI/input/MyInput.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import classes from "./MyInput.module.css";
+const MyInput = React.forwardRef((props, ref) => {
+    return (
+        <input className={classes.myInput} {...props}/>
+    );
+});
+
+export default MyInput;
